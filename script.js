1b9499f8d140d4499195b69a196221a0c9818e59
nilslambertz <nilslambertz@web.de>
2020-06-04 18:43:59 +0200

    added link to github, links are opened in a new tab now

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.getElementById('sortDiv').addEventListener('auxclick', function(e) {
+    if(e.button == 1) {
+        window.open('https://nilslambertz.github.io/SortVisualization/');
+    }
+})
+
 function sortClick() {
-    window.location.href = 'http://sort.nilslambertz.de';
+    window.open('https://nilslambertz.github.io/SortVisualization/');
 }
