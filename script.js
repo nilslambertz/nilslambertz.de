17b2cb71696a4fa04334037502dd4001deb95265
nilslambertz <nilslambertz@web.de>
2020-06-06 01:40:45 +0200

    new design

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('sortDiv').addEventListener('auxclick', function(e) {
-    if(e.button == 1) {
-        window.open('https://nilslambertz.github.io/SortVisualization/');
-    }
-})
-
-function sortClick() {
-    window.open('https://nilslambertz.github.io/SortVisualization/');
-}
