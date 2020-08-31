9a156f7ad8ad5398b044f4ddf191296b580dfd0a
nilslambertz <nilslambertz@web.de>
2020-08-31 02:33:11 +0200

    removed animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,4 @@
 $('document').ready(function () {
-    $("#blackDiv").animate({opacity: 0}, 1000);
-    setTimeout(function() {
-        $("#blackDiv").css("display", "none");
-    },1000);
-
-    $("#welcomeDescription").animate({opacity: 1}, 1000);
+    $("#welcomeDescription").animate({opacity: 1}, 2000);
     $("#welcomeSocials").animate({opacity: 1}, 1000);
 });
