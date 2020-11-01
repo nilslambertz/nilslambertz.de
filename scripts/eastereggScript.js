a812a68ca6f6408f329b4cb163e6ec92b368353f
nilslambertz <nilslambertz@web.de>
2020-11-01 21:26:58 +0100

    changed easteregg-behaviour

--- scripts/eastereggScript.js
+++ scripts/eastereggScript.js
@@ -1,5 +1,5 @@
 let colors = ["#ff00e6", "#ff0000", "#f87100", "#0b7a00", "#00f1f1"];
-let usedColors = ["#255583"]
+let usedColors = ["#255583"];
 let s = 0;
 document.addEventListener('keydown', function(e) {
     if(e.key === "1") {
@@ -9,20 +9,30 @@ document.addEventListener('keydown', function(e) {
             s++;
         } else {
             if(e.key === "7" && s === 3) {
-                if(colors.length === 0) {
-                   colors = Array.from(usedColors);
-                   usedColors = [];
-                }
-                s = 0;
-                let div = document.getElementById("welcomeDiv")
-                let color = colors.splice(Math.floor(Math.random() * colors.length), 1)[0];
-                usedColors.push(color);
-                div.style.backgroundColor = color;
-                console.log("colors: " + colors);
-                console.log("usedColors: " + usedColors);
+                animateBackground();
             } else {
                 s = 0;
             }
         }
     }
 });
+
+function animateBackground() {
+    for(let i = 0; i < colors.length + 1; i++) {
+        setTimeout(() => {
+            changeBackgroundColor();
+        }, 1000 * i);
+    }
+}
+
+function changeBackgroundColor() {
+    if(colors.length === 0) {
+        colors = Array.from(usedColors);
+        usedColors = [];
+    }
+    s = 0;
+    let div = document.getElementById("welcomeDiv")
+    let color = colors.splice(Math.floor(Math.random() * colors.length), 1)[0];
+    usedColors.push(color);
+    div.style.backgroundColor = color;
+}
