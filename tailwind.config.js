85b5612120cebf57540d70d5715bd5a8fe534a9d
nilslambertz <nilslambertz@web.de>
2023-01-25 01:07:51 +0100

    add custom classes for snake game

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         "shadow-gradient":
           "radial-gradient(circle farthest-corner at center, rgba(0,0,0,0) 30%, rgba(0,0,0,1))",
       },
+      gridTemplateColumns: {
+        snake: "repeat(20, 3vw)",
+      },
+      gridTemplateRows: {
+        snake: "repeat(10, 3vw)",
+      },
     },
   },
   plugins: [],
