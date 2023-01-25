db5289ddb4c2b294ecc3cacb504a6ee3050cdd40
nilslambertz <nilslambertz@web.de>
2023-01-25 01:08:22 +0100

    disable global strictMode to avoid errors in development

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     unoptimized: true,
   },
-  assetPrefix: "./",
   trailingSlash: true,
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { ALL_PORTFOLIO_ELEMENTS } from "@/portfolio-elements";
 import Head from "next/head";
 import PortfolioElement from "@/components/PortfolioElement";
 import ShadowOverlay from "@/components/ShadowOverlay";
+import SnakeGame from "@/components/SnakeGame";
+import React from "react";
 
 export default function Home() {
   return (
@@ -12,15 +14,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="w-full h-full">
-        <ShadowOverlay></ShadowOverlay>
-        {ALL_PORTFOLIO_ELEMENTS.map((elem, index, array) => (
-          <PortfolioElement
-            key={index}
-            element={elem}
-            previousElementTitle={array?.[index - 1]?.title}
-            nextElementTitle={array?.[index + 1]?.title}
-          ></PortfolioElement>
-        ))}
+        <React.StrictMode>
+          <ShadowOverlay></ShadowOverlay>
+          {ALL_PORTFOLIO_ELEMENTS.map((elem, index, array) => (
+            <PortfolioElement
+              key={index}
+              element={elem}
+              previousElementTitle={array?.[index - 1]?.title}
+              nextElementTitle={array?.[index + 1]?.title}
+            ></PortfolioElement>
+          ))}
+        </React.StrictMode>
+        <SnakeGame></SnakeGame>
       </main>
     </>
   );
